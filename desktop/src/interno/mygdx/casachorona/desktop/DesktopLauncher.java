9d372a772ee35a480a04adb95dac6a1a5b7a8d57
Luis Fernando Pasquinelli <luispasquinelli@hotmail.com>
2022-05-11 21:43:37 -0300

    Project creation

---
+++ desktop/src/interno/mygdx/casachorona/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package interno.mygdx.casachorona.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import interno.mygdx.casachorona.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
