7bc3803dee48c51698efd3d97494fa6c29c1d8e0
Luis Fernando Pasquinelli <luispasquinelli@hotmail.com>
2022-05-15 21:15:39 -0300

    Settings implemented, with the screen's measures

---
+++ core/src/interno/mygdx/casachorona/game/Settings.java
@@ -0,0 +1,20 @@
+package interno.mygdx.casachorona.game;
+
+public class Settings {
+
+	private static final int SCREEN_WIDTH = 480;
+	private static final int SCREEN_HEIGHT = 320;
+	private static final int SCREEN_SCALE = 2;
+	
+	public static int getScreenWidth() {
+		return SCREEN_WIDTH;
+	}
+	
+	public static int getScreenHeight() {
+		return SCREEN_HEIGHT;
+	}
+	
+	public static int getScreenScale() {
+		return SCREEN_SCALE;
+	}
+}
--- desktop/src/interno/mygdx/casachorona/desktop/DesktopLauncher.java
+++ desktop/src/interno/mygdx/casachorona/desktop/DesktopLauncher.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import interno.mygdx.casachorona.game.Game;
+import interno.mygdx.casachorona.game.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Settings.getScreenWidth() * Settings.getScreenScale();
+		config.height = Settings.getScreenHeight() * Settings.getScreenScale();
 		new LwjglApplication(new Game(), config);
 	}
 }
