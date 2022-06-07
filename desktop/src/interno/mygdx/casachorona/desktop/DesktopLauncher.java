0a28ee419b51866223290705d47bc7f1e7136f9d
Luis Fernando Pasquinelli <luispasquinelli@hotmail.com>
2022-06-07 19:57:45 -0300

    Icons implemented into the game

--- desktop/src/interno/mygdx/casachorona/desktop/DesktopLauncher.java
+++ desktop/src/interno/mygdx/casachorona/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package interno.mygdx.casachorona.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -13,6 +14,9 @@ public class DesktopLauncher {
 		config.height = Settings.SCREEN_HEIGHT * Settings.SCREEN_SCALE;
 		config.resizable = false;
 		config.title = "A Casa Chorona";
+		config.addIcon("graphics/icon/icon128.png", FileType.Internal);
+		config.addIcon("graphics/icon/icon32.png", FileType.Internal);
+		config.addIcon("graphics/icon/icon16.png", FileType.Internal);
 		new LwjglApplication(new CasaChorona(), config);
 	}
 }
